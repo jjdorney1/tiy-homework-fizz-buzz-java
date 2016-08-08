1bf2c2e9c7572d779af6fceda9a9a5fd1bf80f9b
jeffreydorney <jjdorney1@gmail.com>
2016-08-07 22:38:49 -0400

    able to print numbers to the console

---
+++ src/FizzBuzz.java
@@ -0,0 +1,15 @@
+/**
+ * Created by jeffreydorney on 8/5/16.
+ */
+public class FizzBuzz {
+
+    //public int doIt();
+
+    public FizzBuzz(int start, int end) {
+
+        for(int x = start; x <= end; x++){
+            System.out.print( start );
+        }
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by jeffreydorney on 8/5/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        for(int x = 1; x <= 100; x++){
+            System.out.print( x + " " );
+        }
+
+
+
+        //FizzBuzz fizzBuzz = new FizzBuzz(1, 100);
+
+
+    }
+}
