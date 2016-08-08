ba7ac4a1f590cd8f0522aaa66828917416650a24
jeffreydorney <jjdorney1@gmail.com>
2016-08-08 01:11:13 -0400

    Able to get method in place but not able to get doIt method to work

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,13 +3,21 @@
  */
 public class FizzBuzz {
 
-    //public int doIt();
-
     public FizzBuzz(int start, int end) {
-
         for(int x = start; x <= end; x++){
-            System.out.print( start );
+            if(((x % 3) == 0) && ((x % 5) == 0)) {
+                System.out.print( "FizzBuzz ");
+            } else if(( (x % 3) == 0)) {
+                System.out.print( "Fizz ");
+            } else if(( (x % 5) == 0)) {
+                System.out.print( "Buzz ");
+            } else
+                System.out.print( x + " " );
         }
+
+        /*for(int x = start; x <= end; x++){
+            System.out.print( start );
+        }*/
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        for(int x = 1; x <= 100; x++){
-            if(((x % 3) == 0) && ((x % 5) == 0)) {
-                System.out.print( "FizzBuzz ");
-            } else if(( (x % 3) == 0)) {
-                System.out.print( "Fizz ");
-            } else if(( (x % 5) == 0)) {
-                System.out.print( "Buzz ");
-            } else
-            System.out.print( x + " " );
-        }
+        FizzBuzz fizzbuzz = new FizzBuzz(1, 100);
+
+
 
 
 
